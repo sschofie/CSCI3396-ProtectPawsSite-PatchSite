217aebef5660b070575af208069d5ae2191fd23b
Sabrina <swhitney@trinity.edu>
2021-03-25 18:52:35 -0500

    Initial Site Created

---
+++ javascript/login.js
@@ -0,0 +1,25 @@
+let users = new Map();
+
+users['swhitney'] = 'password'
+users['andrew'] = 'ladd'
+
+const form = document.getElementById('loginform');
+const testintButton = document.getElementById('testing');
+
+form.addEventListener('submit', () => {
+    let username = document.getElementById('usernameInput').value;
+    let password = document.getElementById('passwordInput').value;
+
+    if(validateLogin(username, password)) {
+        console.log("success");
+        form.action="file:///C:/Users/sabri/Documents/TrinityUni_2021/CSCI-3396/Project/htmldocs/protectpaws.html"; 
+    } else {
+        console.log("failure");
+    }
+});
+
+function validateLogin(username, password) {
+    if(users[username] === password) {
+        return true;
+    } else return false;
+}
